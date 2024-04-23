cfb0d21d85c26d4c7fa9436cb6b750dde7e6c592
Abbas Al Khalifa <115319928+info-devf5r@users.noreply.github.com>
2024-04-23 13:40:20 -0700

    update Tool-F5R.py

--- Tool-F5R.py
+++ Tool-F5R.py
@@ -1,16 +1,20 @@
+بالطبع! يمكنك إجراء تعديلات في الكود ليتم عرض عناوين IP أيضًا. هنا هو الكود المعدل:
+
+```python
 #!/usr/bin/python
 # Author : Mo3taz Potin
 # @Icanflyy1
-#Account link https://t.me/Icanflyy1
-#Feel free to ask me ...
+# Account link https://t.me/Icanflyy1
+# Feel free to ask me ...
 # version : 1.1
 
-import requests,sys
+import requests
+import sys
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+from socket import gethostbyname
 
-
-print("     __  __            ")            
+print("     __  __            ")
 print("     \ \/ /     _ __ __ _ _   _   ")
 print("      \  /_____| '__/ _` | | | |    ")
 print("      /  \_____| | | (_| | |_| |   ")
@@ -18,8 +22,8 @@ print("     /_/\_\    |_|  \__,_|\__, |    ")
 print("                         |___/       ")
 
 print("   Happy Scanning Script by: Mo3taz Potin")
-print("             @Icanflyy1             	")   
-print("      https://t.me/Icanflyy1   	")  
+print("             @Icanflyy1             	")
+print("      https://t.me/Icanflyy1   	")
 
 # COLORS #
 class bcolors:
@@ -34,79 +38,83 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-if (len(sys.argv) != 2):
-  print("\n Error: Type python3 potin.py yourfile.txt\n")
-  sys.exit()
-
+if len(sys.argv) != 2:
+    print("\n Error: Type python3 potin.py yourfile.txt\n")
+    sys.exit()
 else:
-  f = open(sys.argv[1],'r') 
-  lines = f.readlines()
-  f.close()
+    f = open(sys.argv[1], 'r')
+    lines = f.readlines()
+    f.close()
 
-  filename = 'potin_files/' + sys.argv[1].replace('../','') + '_output.txt'
-  out = open(filename, 'w+')
+    filename = 'potin_files/' + sys.argv[1].replace('../', '') + '_output.txt'
+    out = open(filename, 'w+')
 
-  
+
+def get_ip_address(url):
+    try:
+        ip = gethostbyname(url)
+        return ip
+    except:
+        return 'N/A'
+
+
 for line in lines:
     try:
-      
-      if 'http://' in line.strip() or 'https://' in line.strip():
-        url = line.strip()
-      else:
-        
-        url = 'http://' + line.strip()
-      
-      s = requests.Session()
-      retry = Retry(connect=1, backoff_factor=0.5)
-      
-      s.mount('http://github.com', HTTPAdapter(max_retries=1))
-      r = s.head(url, timeout=1)
-      response = r.headers
-  
-      out.write(url + ':' + str(r.status_code) + '\n')
+        if 'http://' in line.strip() or 'https://' in line.strip():
+            url = line.strip()
+        else:
+            url = 'http://' + line.strip()
+
+        s = requests.Session()
+        retry = Retry(connect=1, backoff_factor=0.5)
+
+        s.mount('http://github.com', HTTPAdapter(max_retries=1))
+        r = s.head(url, timeout=1)
+        response = r.headers
+
+        out.write(url + ':' + str(r.status_code) + '\n')
 
     except requests.ConnectionError as e:
-      print("\n", bcolors.ENDC + url + bcolors.FAIL + " Failed to connect ")
-      continue
+        print("\n", bcolors.ENDC + url + bcolors.FAIL + " Failed to connect ")
+        continue
     except requests.Timeout as e:
         print("[!] : Timeout Error")
         continue
     except requests.RequestException as e:
         print("[!] : General Error")
         continue
-    
-    except KeyboardInterrupt:
-      out.close()
-      print("\nOutput saved in : " + filename + '\n')
-      exit()
-    
-    # Printing the results
-    if (r.status_code == 200):
-    	try:
-        	print("\n", '\x1b[6;30;42m' '[OK]200', bcolors.ENDC, ':' , url,r.headers['server'])
-    	except KeyError:
-    		print('server not found')
-    
-    if (r.status_code == 308):
-    	try:
-    		print("\n", '\x1b[6;39;40m' '[308]',bcolors.OKBLUE, ':' , url,r.headers['server'])
-    	except KeyError:
-    		print('server not found')
-    		    
-    if (r.status_code == 302):
-       		print('\n', bcolors.FAIL, r.status_code, ' : ' , url,r.headers)
 
-    if (r.status_code == 301):
-    	try:
-    		print("\n", '\x1b[6;39;40m' '[301]',bcolors.OKCYAN, ':' , url,r.headers['server'])
-    	except KeyError:
-    		print('server not found')
-    
-    if (r.status_code == 403):
-    	try:
-    		print("\n", '\x1b[6;39;40m' '[403]',bcolors.OKGREEN, ':' , url,r.headers['server'])
-    	except KeyError:
-    		print('server not found')
-    		  
-print("\nOutput saved in : " + filename + '\n')
-out.close()
+    except KeyboardInterrupt:
+        out.close()
+        print("\nOutput saved in : " + filename + '\n')
+        exit()
+
+    # Printing the results
+    if r.status_code == 200:
+        try:
+            print("\n", '\x1b[6;30;42m' '[OK]200', bcolors.ENDC, ':', url, r.headers['server'], get_ip_address(url))
+        except KeyError:
+            print('server not found')
+
+    if r.status_code == 308:
+        try:
+            print("\n", '\x1b[6;39;40m' '[308]', bcolors.OKBLUE, ':', url, r.headers['server'], get_ip_address(url))
+        except KeyError:
+            print('server not found')
+
+    if r.status_code == 302:
+        print('\n', bcolors.FAIL, r.status_code, ' : ', url, r.headers, get_ip_address(url))
+
+    if r.status_code == 301:
+        try:
+            print("\n", '\x1b[6;39;40m' '[301]', bcolors.OKCYAN, ':', url, r.headers['server'], get_ip_address(url))
+        except KeyError:
+            print('server not found')
+
+    if r.status_code == 403:
+        try:
+            print("\n", '\x1b[6;39;40m' '[403]', bcolors.OKGREEN, ':', url, r.headers['server'], get_ip_address(url))
+        except KeyError:
+            print('server not found')
+
+print("\nOutput saved in : " + filename + '\
